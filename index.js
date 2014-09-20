7f5e6950bf4bc2a16ff50d7d7736744849cd0289
Tyler Garrett <tyler@tgrrtt.com>
2014-09-20 16:12:15 -0700

    Graph selects node & link on mouseover

--- index.js
+++ index.js
@@ -1,86 +1,72 @@
-var width = 960,
-    height = 960;
-    fill = d3.scale.category20();
+var w = 960,
+  h = 960,
+  r = 20,
+  fill = d3.scale.category20();
 
-var color = d3.scale.category20();
+var force = d3.layout.force().charge(-500).linkDistance(300).size([w, h]);
 
-var force = d3.layout.force()
-    .charge(-500)
-    .linkDistance(300)
-    .size([width, height]);
+var svg = d3.select("body").append("svg").attr("width", w).attr("height", h);
 
-var fade = function(opacity) {
-  return function(d, i) {
-    ////
+d3.json("pairData.json", function(data) {
 
-    // d is the node object, i is the id, which matches correctly with node obj
-    //fade all non-linked lines
-    svg.selectAll("circle, line").style("opacity", opacity);
-    //console.log(i);
-    // get associated links
-    var relatedLinks = svg.selectAll("line").filter(function(d) {
-      // now d refers to link d
-      // i refers to node id that is being hovered over.
-      if (d.source.id == i || d.target.id == i) {
-        //console.log("yo");
-        return true;
-      } else {
-        return false;
-      }
-      //return d.source.id == i || d.target.id == i;
-    });
-    //console.log(relatedLinks)
+  var json = data;
 
-    relatedLinks.each(function(linkData, iLink) {
+  var link = svg.selectAll("line")
+    .data(json.links).enter()
+    .append("line");
 
-        d3.select(this).style("opacity", 1);
-        console.log(this);
-        console.log(linkData.source);
-        // d3.select(linkData.target);
-    });
-  };
-};
+  var node = svg.selectAll("circle")
+    .data(json.nodes).enter()
+    .append("circle").attr("r", r - 0.75)
+    .style("fill", function(d) {
+      return fill(d.id);
+    })
+    .call(force.drag).on("mouseover", fade(.1)).on("mouseout", fade(1));
 
-var svg = d3.select("body").append("svg")
-    .attr("width", width)
-    .attr("height", height);
+  node.append("title").text(function(d) { return d.name; });
 
-d3.json("pairData.json", function(error, graph) {
-  force
-      .nodes(graph.nodes)
-      .links(graph.links)
-      .start();
+  force.nodes(json.nodes).links(json.links).on("tick", tick).start();
 
-  var link = svg.selectAll(".link")
-      .data(graph.links)
-    .enter().append("line")
-      .attr("class", "link")
-      .style("stroke-width", function(d) { return Math.sqrt(d.value); });
-
-  var node = svg.selectAll(".node")
-      .data(graph.nodes)
-    .enter().append("circle")
-      .attr("class", "node")
-      //.attr("class", function(d) { return d.name; })
-      .attr("cy", 300)
-      .attr("r", 25)
-      .style("fill", function(d) { return fill(d.id); })
-      .call(force.drag)
-      .on("mouseover", fade(.1)).on("mouseout", fade(1));
-
-  node.append("title")
-      .text(function(d) { return d.name; });
-
-  force.on("tick", function() {
-    link.attr("x1", function(d) { return d.source.x; })
-        .attr("y1", function(d) { return d.source.y; })
-        .attr("x2", function(d) { return d.target.x; })
-        .attr("y2", function(d) { return d.target.y; });
-
-    node.attr("cx", function(d) { return d.x; })
-        .attr("cy", function(d) { return d.y; });
+  var linkedByIndex = {};
+  json.links.forEach(function(d) {
+      linkedByIndex[d.source.index + "," + d.target.index] = 1;
   });
-});
 
+  function isConnected(a, b) {
+      return linkedByIndex[a.index + "," + b.index] || linkedByIndex[b.index + "," + a.index] || a.index == b.index;
+  }
+
+  function tick() {
+      node.attr("cx", function(d) {
+          return d.x = Math.max(r, Math.min(w - r, d.x));
+      }).attr("cy", function(d) {
+          return d.y = Math.max(r, Math.min(h - r, d.y));
+      });
+
+      link.attr("x1", function(d) {
+          return d.source.x;
+      }).attr("y1", function(d) {
+          return d.source.y;
+      }).attr("x2", function(d) {
+          return d.target.x;
+      }).attr("y2", function(d) {
+          return d.target.y;
+      });
+  }
+
+  function fade(opacity) {
+      return function(d) {
+          node.style("stroke-opacity", function(o) {
+              thisOpacity = isConnected(d, o) ? 1 : opacity;
+              this.setAttribute('fill-opacity', thisOpacity);
+              return thisOpacity;
+          });
+
+          link.style("stroke-opacity", function(o) {
+              return o.source === d || o.target === d ? 1 : opacity;
+          });
+      };
+  }
+});
 
 
